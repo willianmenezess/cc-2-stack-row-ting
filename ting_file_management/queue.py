3e8cc138846e19869669c600ba48c557dbbfc2be
willianmenezess <willmenezes@outlook.com.br>
2023-09-28 16:04:47 -0300

    feat: resolve requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -2,19 +2,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 
 class Queue(AbstractQueue):
+    """Implementação da ED Fila usando list"""
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.__data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.__data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.__data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.__data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
-
-# commit inicial
+        if index not in range(len(self.__data)):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.__data[index]
