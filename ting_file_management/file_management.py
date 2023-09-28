070c27210cdc4d0fa03450631ceeb6c492695cbb
willianmenezess <willmenezes@outlook.com.br>
2023-09-28 17:42:38 -0300

    feat: resolve requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.split(".")[-1] != "txt":
+        sys.stderr.write("Formato inválido")
+
+    try:
+        with open(path_file, "r") as file:
+            txt_read = file.readlines()
+            list_lines_txt = [line.strip() for line in txt_read]
+            return list_lines_txt
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+
+# if __name__ == "__main__":
+#     path_file = "test.txt"
+#     print(txt_importer(path_file))
