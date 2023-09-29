7a2b3d90763b299c81473a32424cad7dd98bbf02
willianmenezess <willmenezes@outlook.com.br>
2023-09-29 16:37:12 -0300

    refactor

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,41 +1,40 @@
 from ting_file_management.file_management import txt_importer
 from queue import Queue
-from sys import stdout
 
 
 def process(path_file, instance):
     """Transforma o retorno da função txt_importer em um
     dicionário que será armazenado dentro da Queue"""
+    for q in instance._data:
+        if q['nome_do_arquivo'] == path_file:
+            return None
+
     list_lines_txt = txt_importer(path_file)
     new_enqueue = {
         'nome_do_arquivo': path_file,
         'qtd_linhas': len(list_lines_txt),
         'linhas_do_arquivo': list_lines_txt,
     }
-    if instance.get_element(new_enqueue):
-        return None
-
-    # adicionando o dicionário na fila
     instance.enqueue(new_enqueue)
-    print(new_enqueue, file=stdout)
+    print(new_enqueue)
 
 
 def remove(instance):
     if instance.is_empty():
-        return stdout.write('Não há elementos\n')
+        return print('Não há elementos')
     else:
         fileDequeued = instance.dequeue()
-        return stdout.write(
-          f"Arquivo {fileDequeued['nome_do_arquivo']} removido com sucesso\n")
+        return print(
+          f"Arquivo {fileDequeued['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
     try:
-        stdout.write(
-            f"{instance.search(position)}\n"
+        return print(
+            f"{instance.search(position)}"
         )
     except IndexError:
-        stdout.write("Posição inválida ou inexistente\n")
+        return print("Posição inválida")
 
 
 if __name__ == "__main__":
@@ -43,4 +42,4 @@ if __name__ == "__main__":
     path_file = "statics/arquivo_teste.txt"
     process(path_file, instance)
     # remove(instance)
-    file_metadata(instance, 0)
+    file_metadata(instance, 1)
